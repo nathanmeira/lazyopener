c01767c5bc48636e40925ed28b823aa1dd88779c
nathan-at-560491377201 <KZ4W//9qu3c5iD2N2k4GH0frg5BjAQ+a5sYvlLo3oac=>
2021-04-23 20:59:41 -0300

    First version

---
+++ js/script.js
@@ -0,0 +1,87 @@
+let ac = {
+  init : function () {
+    ac.chr = document.getElementById("chr");
+    ac.cmin = document.getElementById("cmin");
+    ac.csec = document.getElementById("csec");
+
+    ac.thr = ac.createSel(23);
+    document.getElementById("tpick-h").appendChild(ac.thr);
+    ac.thm = ac.createSel(59);
+    document.getElementById("tpick-m").appendChild(ac.thm);
+    ac.ths = ac.createSel(59);
+    document.getElementById("tpick-s").appendChild(ac.ths);
+
+    ac.tset = document.getElementById("tset");
+    ac.tset.addEventListener("click", ac.set);
+    ac.treset = document.getElementById("treset");
+    ac.treset.addEventListener("click", ac.reset);
+
+    ac.alarm = null;
+    setInterval(ac.tick, 1000);
+  },
+
+  createSel : function (max) {
+    let selector = document.createElement("select");
+    for (let i=0; i<=max; i++) {
+      let opt = document.createElement("option");
+      i = ac.padzero(i);
+      opt.value = i;
+      opt.innerHTML = i;
+      selector.appendChild(opt);
+    }
+    return selector
+  },
+
+  padzero : function (num) {
+    if (num < 10) { num = "0" + num; }
+    else { num = num.toString(); }
+    return num;
+  },
+
+  tick : function () {
+    let now = new Date();
+    let hr = ac.padzero(now.getHours());
+    let min = ac.padzero(now.getMinutes());
+    let sec = ac.padzero(now.getSeconds());
+
+    ac.chr.innerHTML = hr;
+    ac.cmin.innerHTML = min;
+    ac.csec.innerHTML = sec;
+
+
+
+    let url = document.getElementById("url").value;
+
+    if (ac.alarm != null) {
+      now = hr + min + sec;
+      if (now == ac.alarm) {
+        window.open(
+          url, "_blank");
+      }
+    }
+  },
+
+  set : function () {
+    ac.alarm = ac.thr.value + ac.thm.value + ac.ths.value;
+    ac.thr.disabled = true;
+    ac.thm.disabled = true;
+    ac.ths.disabled = true;
+    ac.tset.disabled = true;
+    ac.treset.disabled = false;
+  },
+
+  reset : function () {
+    ac.alarm = null;
+    ac.thr.disabled = false;
+    ac.thm.disabled = false;
+    ac.ths.disabled = false;
+    ac.tset.disabled = false;
+    ac.treset.disabled = true;
+    let url = document.getElementById("url");
+    url.classList.remove("check");
+    url.value("");
+    document.getElementById("url").value = "";
+  }
+};
+
+window.addEventListener("load", ac.init);
